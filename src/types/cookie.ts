6b0c3e8e45b3735db6834a53e880553d77635de6
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-07-03 03:34:39 +0900

    feat: add cookie type

---
+++ src/types/cookie.ts
@@ -0,0 +1,9 @@
+export type getterProps = string;
+export type getterReturn = string | null;
+export type cookieParts = any[];
+
+export type setterProps = {
+  name: string;
+  value: string;
+  options: Record<string, any>;
+};
