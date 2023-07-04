0f3b79214eb9ad416feca21b93015928c47f9986
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-07-04 20:31:44 +0900

    feat: add Cookie Options type

--- src/types/cookie.ts
+++ src/types/cookie.ts
@@ -1,8 +1,13 @@
 export type getterNameProps = string;
 export type getterReturn = string | null;
-export type cookieParts = string[];
 
 export type setterNameProps = string;
 export type setterValueProps = string;
-export type setterOptionProps = Record<string, any>;
+export interface setterOptionProps {
+  expires?: Date;
+  path?: string;
+  domain?: string;
+  secure?: string;
+  sameSite?: 'None' | 'Lax' | 'Strict';
+}
 export type setterReturn = void;
